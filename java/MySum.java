c1c81e23b61a55a85f6d9fe97a957515cd4e297e
unknown <yatharth.sharma@publicissapient.comgit commitgit config --global user.email yatharth.sharma@publicissapient.com>
2021-04-26 15:40:34 +0530

    subtract and branch added

--- java/MySum.java
+++ java/MySum.java
@@ -1,9 +1,11 @@
 public class MySum{
 public static void main(String args[]){
-int a = 3;
+int a = 5;
 int b = 4;
 int sum = a+b;
+int minus = a-b;
 System.out.println(sum);
+System.out.println(minus);
 }
 
 
