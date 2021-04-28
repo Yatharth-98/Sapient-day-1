24077359d9943e8c79c7d3df0fa5c87a05e0cb60
unknown <yatharth.sharma@publicissapient.comgit commitgit config --global user.email yatharth.sharma@publicissapient.com>
2021-04-28 09:16:04 +0530

    Calculator added

---
+++ src/main/java/MyCalculator.java
@@ -0,0 +1,14 @@
+public class MyCalculator{
+public static void main(String args[]){
+int a =4;
+int b =2;
+int sum = a+b;
+int minus = a-b;
+int mul = a*b;
+int Div = a/b;
+System.out.println(sum);
+System.out.println(minus);
+System.out.println(Div);
+System.out.println(mul);
+}
+}
